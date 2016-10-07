861ee3543da01198248f5dcadbee05774e0d958d
Aluno <Aluno@192.168.105.4>
2016-10-07 10:21:11 -0300

    Crei Hello World.

--- src/exemplogit/ExemploGit.java
+++ src/exemplogit/ExemploGit.java
@@ -15,7 +15,7 @@ public class ExemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Olá mundo!");
     }
     
 }
